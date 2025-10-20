f83a5b473928956a2450c9c8874628090a61381a
origin <acmuhammadmustafa@gmail.com>
2025-10-20 13:32:50 -0400

    created room and used in main

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,17 +1,26 @@
 package com.pluralsight;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        ArrayList<Room>rooms = new ArrayList<>();
+
+        Room r101 = new Room(1);
+        Room r102 = new Room(2);
+        Room r103 = new Room(2);
+        Room r104 = new Room(2);
+
+        rooms.add(r101);
+        rooms.add(r102);
+        rooms.add(r103);
+        rooms.add(r104);
+
+        for(Room r : rooms){
+            System.out.println(r);
         }
+
     }
+
 }
---
+++ src/main/java/com/pluralsight/Room.java
@@ -0,0 +1,52 @@
+package com.pluralsight;
+
+public class Room {
+
+    private int numberOfBeds;
+    private double price;
+    private boolean isOccupied;
+    private boolean isDirty;
+
+    public Room(int numberOfBeds) {
+        this.numberOfBeds = numberOfBeds;
+        this.price = 120.00;
+        this.isOccupied = false;
+        this.isDirty = false;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public boolean isDirty() {
+        return isDirty;
+    }
+
+    public boolean isAvailable() {
+        return !(isOccupied || isDirty);
+//        if (isOccupied || isDirty){
+//            return false;
+//    }
+//        else{
+//            return true;
+//        }
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "numberOfBeds=" + numberOfBeds +
+                ", price=" + price +
+                ", isOccupied=" + isOccupied +
+                ", isDirty=" + isDirty +
+                '}';
+    }
+}
